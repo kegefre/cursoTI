5df939b4c059080bb865dbbffcfbee2e06eeadea
kellisgf <kellisgf@gmail.com>
2021-08-29 23:58:38 -0300

    Aula 3 Atualizada

--- ciclo2/aula5/aulaJS5/ceps.js
+++ ciclo2/aula5/aulaJS5/ceps.js
@@ -4,10 +4,23 @@ window.onload=function(){
 	const cxCep = document.querySelector('.cxCep');
 	const btnBuscaCep = document.querySelector('#btnBuscaCep');
 	
+	const dadosCep = async function(cepUrl){
+		var url=`https://viacep.com.br/ws/${cepUrl}/json/`;
+		var consultaCep = await fetch(url);
+		var dadosCep = await consultaCep.json();
+		//console.log(consultaCep);
+		for ( var campos in dadosCep){
+			//console.log(campos);
+			if(document.querySelector("#"+campos)!=null){
+				document.querySelector("#"+campos).value=dadosCep[campos];
+			}
+
+		}
+	}
 
 	btnBuscaCep.addEventListener('click',()=>{
-		var url=`https://viacep.com.br/ws/${cxCep.value}/jason`;
-		alert(url);
+		let cep = cxCep.value;
+		dadosCep(cep);
 	})
 
 }
